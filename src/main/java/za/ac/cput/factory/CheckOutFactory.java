9c953840008089897a7549be8255e81b68e5cf8a
ItsPxngea <michaeljohnson5143@gmail.com>
2023-04-05 21:54:57 +0200

    Test Files: ItsPxngea

--- src/main/java/za/ac/cput/factory/CheckOutFactory.java
+++ src/main/java/za/ac/cput/factory/CheckOutFactory.java
@@ -4,20 +4,21 @@ import za.ac.cput.domain.CheckOut;
 import za.ac.cput.util.Helper;
 
 public class CheckOutFactory {
-    public static CheckOut checkOut(String checkOutID,String customerID,String productID,String productName,String productQuantity,double productPrice,double totalPrice, double tax){
-        if (Helper.isNullOrEmpty(checkOutID) || Helper.isNullOrEmpty(customerID) || Helper.isNullOrEmpty(productID) || Helper.isNullOrEmpty(productName) || Helper.isInvalidDouble(productPrice) || Helper.isInvalidDouble(totalPrice) || Helper.isInvalidDouble(tax)){
+    public static CheckOut buildCheckOut(String productName,int productQuantity,double productPrice,double totalPrice, double tax){
+        if ( Helper.isNullOrEmpty(productName) || Helper.isInvalidInt(productQuantity) || Helper.isInvalidDouble(productPrice) || Helper.isInvalidDouble(totalPrice) || Helper.isInvalidDouble(tax)){
             return null;
         }
 
         String firstName = "Jeff";
         String lastName = "Bezos";
-        checkOutID = Helper.generateID();
-        customerID = Helper.generateCustomerID(firstName, lastName);
-        productID = Helper.generateID();
+        String checkOutID = Helper.generateID();
+        String customerID = Helper.generateCustomerID(firstName, lastName);
+        String productID = Helper.generateID();
 
         return new CheckOut.Builder().setCheckOutID(checkOutID)
                 .setCustomerID(customerID)
                 .setProductID(productID)
+                .setProductQuantity(productQuantity)
                 .setProductName(productName)
                 .setProductPrice(productPrice)
                 .setTotalPrice(totalPrice)
---
+++ src/test/java/za/ac/cput/factory/PreBuiltFactoryTest.java
@@ -0,0 +1,68 @@
+package java.za.ac.cput.factory;
+
+//import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.PreBuilt;
+import za.ac.cput.factory.PreBuiltFactory;
+
+class PreBuiltFactoryTest {
+
+    @Test
+    void createPreBuilt() {
+        PreBuilt p = PreBuiltFactory.buildPreBuilt("GTX500", "Your product is good.","5 Stars","Graphics Card","Gaming","Valid", 4590);
+        System.out.println(p.toString());
+        assertNotNull(p);
+    }
+    @Test
+    void emptyParameters(){
+        PreBuilt p = PreBuiltFactory.buildPreBuilt("", "Your product is good.","5 Stars","Graphics Card","Gaming","Valid", 4590);
+        assertNull(p);
+    }
+    @Test
+    void emptyParameters2(){
+        PreBuilt p = PreBuiltFactory.buildPreBuilt("GTX500", "Your product is good.","5 Stars","","Gaming","Valid", 4590);
+        assertNull(p);
+    }
+
+    @Test
+    void emptyParameters(){
+        PreBuilt p = PreBuiltFactory.buildPreBuilt("GTX500", "Your product is good.","5 Stars","Graphics Card","","Valid", 4590);
+        assertNull(p);
+    }
+
+    @Test
+    void emptyParameters3(){
+        PreBuilt p = PreBuiltFactory.buildPreBuilt("GTX500", "Your product is good.","5 Stars","Graphics Card","Gaming","", 4590);
+        assertNull(p);
+    }
+
+    @Test
+    void invalidParameters(){
+        PreBuilt p = PreBuiltFactory.buildPreBuilt("GTX500", "Your product is good.","5 Stars","Graphics Card","Gaming","Valid", -4590);
+        assertNull(p);
+    }
+
+    @Test
+    void testEquality(){
+        PreBuilt p = PreBuiltFactory.buildPreBuilt("GTX500", "Your product is good.","5 Stars","Graphics Card","Gaming","Valid", 4590);
+
+        PreBuilt p2 = PreBuiltFactory.buildPreBuilt("GTX600", "Your product is okay.","5 Stars","Graphics Card","Gaming","Valid", 4400);
+
+        System.out.println(p.toString());
+        System.out.println(p2.toString());
+        assertNotEquals(p,p2);
+    }
+
+    @Test
+    void testEquality2(){
+        PreBuilt p = PreBuiltFactory.buildPreBuilt("GTX500", "Your product is good.","5 Stars","Graphics Card","Gaming","Valid", 4590);
+
+        PreBuilt p2 = p;
+
+        System.out.println(p.toString());
+        assertNotEquals(p,p2);
+
+    }
+
+
+}
