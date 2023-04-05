5ac9aac1725207bddca0c6e30b629af3bcaf3608
Reece Bergstedt <106551701+reecethebeat@users.noreply.github.com>
2023-04-05 22:24:47 +0200

    Bundle and Product Repository tests added

---
+++ src/test/java/za/ac/cput/interfaces/ProductRepositoryTest.java
@@ -0,0 +1,33 @@
+package za.ac.cput.interfaces;
+/* ProductRepositoryTest.java
+ Entity for the ProductRepositoryTest
+ Author: Reece Bergstedt - 221075240
+ Date: 22 March 2023
+*/
+import static org.junit.jupiter.api.Assertions.*;
+public class ProductRepositoryTest {
+
+    @org.junit.jupiter.api.Test
+    void getRepository() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void create() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void read() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAll() {
+    }
+}
