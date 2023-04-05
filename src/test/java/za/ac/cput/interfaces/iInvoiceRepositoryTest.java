b1838efd01915500cb5278058d968a037c7a0784
Draai <StevenAlexanderDraai@gmail.com>
2023-04-06 00:19:53 +0200

    Added repository tests.

---
+++ src/test/java/za/ac/cput/interfaces/iInvoiceRepositoryTest.java
@@ -0,0 +1,29 @@
+package za.ac.cput.interfaces;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class iInvoiceRepositoryTest {
+    @org.junit.jupiter.api.Test
+    void getRepository() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void create() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void read() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAll() {
+    }
+}
