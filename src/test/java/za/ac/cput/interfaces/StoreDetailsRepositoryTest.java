ec538c1daaef53f047ae16628af2ef9d9965bc70
Habbo420 <100413007+Habbo420@users.noreply.github.com>
2023-04-06 19:20:45 +0200

    Update StoreDetailsRepositoryTest.java

--- src/test/java/za/ac/cput/interfaces/StoreDetailsRepositoryTest.java
+++ src/test/java/za/ac/cput/interfaces/StoreDetailsRepositoryTest.java
@@ -3,31 +3,33 @@ package za.ac.cput.interfaces;
 Author: Hanno Visser Immelman 221074414
  */
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 import za.ac.cput.domain.StoreDetails;
 import za.ac.cput.factory.StoreDetailsFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@TestMethodOrder(MethodOrderer.MethodName.class)
 class StoreDetailsRepositoryTest {
     private static StoreDetailsRepository repository = StoreDetailsRepository.getRepository();
     private static StoreDetails sd = StoreDetailsFactory.buildStoreDetails("M computers","69 Nice Street, Cape Town","0988422548","MComp@gmail.com");
 
     @org.junit.jupiter.api.Test
-    void create() {
+    void a_create() {
         StoreDetails createSD = repository.create(sd);
         assertEquals(sd.getStoreID(),createSD.getStoreID());
         System.out.println("Create: " + createSD);
     }
 
     @org.junit.jupiter.api.Test
-    void read() {
+    void b_read() {
         StoreDetails readSD = repository.read(sd.getStoreID());
         assertNotNull(readSD);
         System.out.println("Read: " + readSD);
     }
 
     @org.junit.jupiter.api.Test
-    void update() {
+    void c_update() {
         StoreDetails updateSD = new StoreDetails.Builder().copy(sd)
                 .setStoreTel("072589652")
                 .build();
@@ -36,14 +38,14 @@ class StoreDetailsRepositoryTest {
     }
 
     @org.junit.jupiter.api.Test
-    void delete() {
+    void e_delete() {
         boolean suc = repository.delete(sd.getStoreID());
         assertTrue(suc);
         System.out.println("Deleted: " + suc);
     }
 
     @org.junit.jupiter.api.Test
-    void getAll() {
+    void d_getAll() {
         System.out.println("Show everything: ");
         System.out.println(repository.getAll());
     }
