f7df46ae42138cb63d9fddd29649030b3c7e271d
Habbo420 <100413007+Habbo420@users.noreply.github.com>
2023-04-06 16:53:21 +0200

    Update StoreDetailsRepositoryTest.java

--- src/test/java/za/ac/cput/interfaces/StoreDetailsRepositoryTest.java
+++ src/test/java/za/ac/cput/interfaces/StoreDetailsRepositoryTest.java
@@ -1,29 +1,50 @@
 package za.ac.cput.interfaces;
+/*
+Author: Hanno Visser Immelman 221074414
+ */
+
+import za.ac.cput.domain.StoreDetails;
+import za.ac.cput.factory.StoreDetailsFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StoreDetailsRepositoryTest {
-    @org.junit.jupiter.api.Test
-    void getRepository() {
-    }
+    private static StoreDetailsRepository repository = StoreDetailsRepository.getRepository();
+    private static StoreDetails sd = StoreDetailsFactory.buildStoreDetails("M computers","69 Nice Street, Cape Town","0988422548","MComp@gmail.com")
 
     @org.junit.jupiter.api.Test
     void create() {
+        StoreDetails createSD = repository.create(sd);
+        assertEquals(sd.getStoreID(),createSD.getStoreID());
+        System.out.println("Create: " + createSD);
     }
 
     @org.junit.jupiter.api.Test
     void read() {
+        StoreDetails readSD = repository.read(sd.getStoreID());
+        assertNotNull(readSD);
+        System.out.println("Read: " + readSD);
     }
 
     @org.junit.jupiter.api.Test
     void update() {
+        StoreDetails updateSD = new StoreDetails.Builder().copy(sd)
+                .setStoreTel("072589652")
+                .build();
+        assertNotNull(repository.update(updateSD));
+        System.out.println("Updated: " + updateSD);
     }
 
     @org.junit.jupiter.api.Test
     void delete() {
+        boolean suc = repository.delete(sd.getStoreID());
+        assertTrue(suc);
+        System.out.println("Deleted: " + suc);
     }
 
     @org.junit.jupiter.api.Test
     void getAll() {
+        System.out.println("Show everything: ");
+        System.out.println(repository.getAll());
     }
 }
