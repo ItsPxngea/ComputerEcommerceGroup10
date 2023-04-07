bc43cd7ed42d960f1d48f633f824f81b79a22f33
Reece Bergstedt <106551701+reecethebeat@users.noreply.github.com>
2023-04-07 14:55:52 +0200

    Add files via upload

--- src/test/java/za/ac/cput/interfaces/ProductRepositoryTest.java
+++ src/test/java/za/ac/cput/interfaces/ProductRepositoryTest.java
@@ -5,29 +5,51 @@ package za.ac.cput.interfaces;
      Date: 22 March 2023
 */
 import static org.junit.jupiter.api.Assertions.*;
-public class ProductRepositoryTest {
+import org.junit.jupiter.api.*;
+import za.ac.cput.domain.Product;
+import za.ac.cput.factory.ProductFactory;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class ProductRepositoryTest {
+
+    private static ProductRepository repository = ProductRepository.getRepository();
+    private static Product product = ProductFactory.buildProduct("RTX 3060 TI", "Graphics Card", "Next Generation gaming with the RTX 3060 TI",
+            1, "Nvidea", "1", "Gaming", "Great Product, Great Service","06-04-2023","4/5", 3000.00);
 
     @org.junit.jupiter.api.Test
-    void getRepository() {
+    void a_create() {
+        Product created = repository.create(product);
+        assertEquals(product.getProductID(), created.getProductID());
+        System.out.println("Create: " + created);
     }
 
     @org.junit.jupiter.api.Test
-    void create() {
+    void c_read() {
+        Product read = repository.read(product.getProductID());
+        assertNotNull(read);
+        System.out.println("Read: " + read);
     }
 
     @org.junit.jupiter.api.Test
-    void read() {
+    void d_update() {
+        Product updated = new Product.Builder().copy(product)
+                .setProductQuantity(2)
+                .setProductPrice(6000)
+                .build();
+        assertNotNull(repository.update(updated));
+        System.out.println("Updated: " + updated);
     }
 
     @org.junit.jupiter.api.Test
-    void update() {
+    void f_delete() {
+        boolean success = repository.delete(product.getProductID());
+        assertTrue(success);
+        System.out.println("Deleted: " + success);
     }
 
     @org.junit.jupiter.api.Test
-    void delete() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getAll() {
+    void e_getAll() {
+        System.out.println("Show all: ");
+        System.out.println(repository.getAll());
     }
 }
