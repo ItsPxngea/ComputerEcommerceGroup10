3de88efa3516411627e2bfc994822f972b6c4a20
Habbo420 <100413007+Habbo420@users.noreply.github.com>
2023-06-10 15:25:37 +0200

    Added service testing

---
+++ src/test/java/za/ac/cput/service/impl/StoreDetailsServiceImplTest.java
@@ -0,0 +1,50 @@
+package za.ac.cput.service.impl;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.domain.StoreDetails;
+import za.ac.cput.factory.StoreDetailsFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class StoreDetailsServiceImplTest{
+    private static StoreDetailsServiceImpl service = StoreDetailsServiceImpl.getService();
+    private static StoreDetails storeDetails = StoreDetailsFactory.buildStoreDetails("M computers","69 Nice Street, Cape Town","0988422548","MComp@gmail.com");
+
+    @Test
+    void a_create(){
+        StoreDetails created = service.create(storeDetails);
+        assertEquals(storeDetails.getStoreID(), created.getStoreID());
+        System.out.println();
+    }
+
+    @Test
+    void b_read(){
+        StoreDetails read = service.read(storeDetails);
+        assertNotNull(read);
+        System.out.println("Read: "+read);
+    }
+
+    @Test
+    void c_update(){
+        StoreDetails updated = new StoreDetails.Builder().copy(storeDetails)
+                .setStoreName("N Computers")
+                .build();
+        assertNotNull(service.update(updated));
+        System.out.println("Updated: "+updated);
+    }
+
+    @Test
+    void e_delete(){
+        boolean success = service.delete(storeDetails);
+        assertTrue(success);
+        System.out.println("Deleted: " + success);
+    }
+
+    @Test
+    void d_getAll(){
+        System.out.println("Show all: "+service.getAll());
+    }
+}
