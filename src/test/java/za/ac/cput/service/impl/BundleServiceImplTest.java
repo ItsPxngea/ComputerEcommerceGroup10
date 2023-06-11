9b6d4142309b94fa09c4a94fee2ddf9e66230278
Reece Bergstedt <106551701+reecethebeat@users.noreply.github.com>
2023-06-11 15:17:42 +0200

    Bundle and product service test

---
+++ src/test/java/za/ac/cput/service/impl/BundleServiceImplTest.java
@@ -0,0 +1,59 @@
+package za.ac.cput.service.impl;
+/* BundleServiceImpl.java
+ Entity for the ProductService
+ Author: Reece Bergstedt - 221075240
+ Date: 10 June 2023
+*/
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.domain.Bundle;
+import za.ac.cput.factory.BundleFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class BundleServiceImplTest {
+
+    private static BundleServiceImpl bundleservice = BundleServiceImpl.getService();
+
+    private static Bundle bundle = BundleFactory.buildBundle("Ryzen Bundle", "Gaming Pc", "Complete ryzen only pc",
+            1, "2 Years", "Gaming", "Great System, Great Service", "05-04-2023","4/5",2000.00, "Ryzen Setup");
+
+    @Test
+    void a_create() {
+        Bundle created = bundleservice.create(bundle);
+        assertEquals(bundle.getBundleID(), created.getBundleID());
+        System.out.println("Create: " + created);
+    }
+
+    @Test
+    void b_read() {
+        Bundle read =  bundleservice.read(bundle);
+        assertNotNull(read);
+        System.out.println("Read: " + read);
+    }
+
+    @Test
+    void c_update() {
+        Bundle updated = new Bundle.Builder().copy(bundle)
+                .setBundleQuantity(2)
+                .setBundlePrice(4000)
+                .build();
+        assertNotNull( bundleservice.update(updated));
+        System.out.println("Updated: " + updated);
+    }
+
+    @Test
+    void e_delete() {
+        boolean success = bundleservice.delete(bundle);
+        assertTrue(success);
+        System.out.println("Deleted: " + success);
+    }
+
+    @Test
+    void d_getAll() {
+        System.out.println("Show all: ");
+        System.out.println(bundleservice.getAll());
+    }
+}
