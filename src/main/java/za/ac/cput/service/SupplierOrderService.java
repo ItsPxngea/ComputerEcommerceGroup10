e48d0476520720d2ef7110beb4997636fb9566a2
ethan <220101299@mycput.ac.za>
2023-06-11 19:36:47 +0200

    Supplier and supplierOrder: Service and service implementation

--- src/main/java/za/ac/cput/service/SupplierOrderService.java
+++ src/main/java/za/ac/cput/service/SupplierOrderService.java
@@ -1,5 +1,10 @@
 package za.ac.cput.service;
 
+/*
+Ethan Botes - 220101299
+This is the SupplierOrderService Interface
+Date: 11 - 060 2023
+ */
 
 import za.ac.cput.domain.SupplierOrder;
 
--- src/main/java/za/ac/cput/service/SupplierService.java
+++ src/main/java/za/ac/cput/service/SupplierService.java
@@ -1,5 +1,11 @@
 package za.ac.cput.service;
 
+/*
+Ethan Botes - 220101299
+This is the SupplierService interface
+Date: 11 - 060 2023
+ */
+
 import za.ac.cput.domain.Supplier;
 
 import java.util.Set;
