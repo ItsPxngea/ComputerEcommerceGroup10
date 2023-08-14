0b86095fce94172efeea9595ca66eae2567a1428
ItsPxngea <michaeljohnson5143@gmail.com>
2023-08-14 18:18:05 +0200

    Created Address, City and Country Domain

---
+++ src/main/java/za/ac/cput/domain/Country.java
@@ -0,0 +1,76 @@
+package za.ac.cput.domain;
+
+import java.util.Objects;
+
+public class Country {
+    public String ID;
+    public String countryName;
+
+    public Country() {
+    }
+
+    public Country(String ID, String countryName) {
+        this.ID = ID;
+        this.countryName = countryName;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Country country)) return false;
+        return Objects.equals(getID(), country.getID()) && Objects.equals(getCountryName(), country.getCountryName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getID(), getCountryName());
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "ID='" + ID + '\'' +
+                ", countryName='" + countryName + '\'' +
+                '}';
+    }
+
+    private Country(Country.Builder builder) {
+        this.ID = builder.ID;
+        this.countryName = builder.countryName;
+    }
+
+    public static class Builder {
+        public String ID;
+        public String countryName;
+
+        public Builder setID(String ID) {
+            this.ID = ID;
+            return this;
+        }
+
+        public Builder setCountryName(String countryName) {
+            this.countryName = countryName;
+            return this;
+        }
+
+        public Country.Builder copy(Country country) {
+            this.ID = country.ID;
+            this.countryName = country.countryName;
+            return this;
+        }
+
+        public Country build() {
+            return new Country(this);
+        }
+
+    }
+
+}
