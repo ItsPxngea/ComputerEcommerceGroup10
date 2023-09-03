fe32ac09d68f275c2aaef0fe44396cc46f9f88d5
Reece Bergstedt <106551701+reecethebeat@users.noreply.github.com>
2023-09-03 12:23:35 +0200

    Added review Service

---
+++ src/main/java/za/ac/cput/service/ReviewService.java
@@ -0,0 +1,11 @@
+package za.ac.cput.service;
+
+
+
+import za.ac.cput.domain.Review;
+
+import java.util.List;
+
+public interface ReviewService extends IService<Review, String>{
+    List<Review> getAll();
+}
