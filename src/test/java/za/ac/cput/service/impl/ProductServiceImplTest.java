419a2cc59c8b0c1bac40ef790cee875e38b334d6
Reece Bergstedt <106551701+reecethebeat@users.noreply.github.com>
2023-09-03 12:27:04 +0200

    Product and Review service test

--- src/test/java/za/ac/cput/service/impl/ProductServiceImplTest.java
+++ src/test/java/za/ac/cput/service/impl/ProductServiceImplTest.java
@@ -4,14 +4,21 @@ package za.ac.cput.service.impl;
  Author: Reece Bergstedt - 221075240
  Date: 10 June 2023
 */
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.domain.Customer;
 import za.ac.cput.domain.Product;
+import za.ac.cput.domain.Sales;
+import za.ac.cput.domain.SalesItem;
+import za.ac.cput.factory.CustomerFactory;
 import za.ac.cput.factory.ProductFactory;
+import za.ac.cput.factory.SalesFactory;
+import za.ac.cput.factory.SalesItemFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,41 +27,78 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductServiceImplTest {
 
     @Autowired
-    private static ProductServiceImpl service;
+    private ProductServiceImpl service;
 
-    private static final Product product = ProductFactory.buildProduct("RX 4050", "Item", "Next Generation gaming with the RTX 4050", 4800.00);
+
+    private static final List<Product> products = Arrays.asList(
+            ProductFactory.buildProduct("RTX 3060 TI", "Item", "Next Generation gaming with the RTX 4050", 4800.00),
+            ProductFactory.buildProduct("FX 950", "Item", "Next Generation gaming with the RTX 950", 4000.00),
+                    ProductFactory.buildProduct("RX 4050", "Item", "Next Generation gaming with the RTX 4050", 4800.00)
+                    );
+    private static final Customer customer = CustomerFactory.buildTestCustomer(
+            "Test2456",
+            "Luke",
+            "Ben",
+            "LW@gmail.com",
+            "wufh%2465"
+    );
+
+    private static final Sales sales = SalesFactory.buildSales(
+            "16-08-2023",
+            4560.00,
+            customer
+    );
+    private static final SalesItem salesItem = SalesItemFactory.buildSales(sales, products, 4, 4800);
 
     @Test
+    @Transactional
     void a_create() {
-        Product created = service.create(product);
-        System.out.println("Create: " + created);
+
+        for (Product product : products) {
+            Product created = service.create(product);
+            System.out.println("Create: " + created);
+        }
     }
 
     @Test
+    @Transactional
     void b_read() {
-        Product read = service.read(product.getProductID());
-        assertNotNull(read);
-        System.out.println("Read: " + read);
+
+        for (Product product : products) {
+            Product read = service.read(product.getProductID());
+            assertNotNull(read);
+            System.out.println("Read: " + read);
+        }
+
     }
 
     @Test
+    @Transactional
     void c_update() {
-        Product updated = new Product.Builder().copy(product)
-                .setProductPrice(6000)
-                .build();
-        assertNotNull(service.update(updated));
-        System.out.println("Updated: " + updated);
+
+        for (Product product : products) {
+            Product updated = new Product.Builder()
+                    .copy(product)
+                    .setProductPrice(6000)
+                    .build();
+
+            assertNotNull(updated);
+
+            // Perform the update operation here, for example:
+            Product updatedProduct = service.update(updated);
+
+            System.out.println("Updated: " + updatedProduct);
+        }
     }
 
     @Test
     @Disabled
-    void e_delete() {
-        boolean success = service.delete(product.getProductID());
-        assertTrue(success);
-        System.out.println("Deleted: " + success);
+    void delete() {
+
     }
 
     @Test
+    @Transactional
     void d_getAll() {
         System.out.println("Show all: ");
         System.out.println(service.getAll());
