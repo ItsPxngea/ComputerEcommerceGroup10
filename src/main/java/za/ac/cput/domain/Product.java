50527510055e92c0a0cfde29b6ecbe5b0b8192e3
Reece Bergstedt <106551701+reecethebeat@users.noreply.github.com>
2023-09-03 12:19:29 +0200

    Improved Product and added review

--- src/main/java/za/ac/cput/domain/Product.java
+++ src/main/java/za/ac/cput/domain/Product.java
@@ -20,16 +20,26 @@ public class Product implements Serializable {
     public String productID;
 
     public String productName;
-    private String productType;
-    private String productDescription;
-    private double productPrice;
-
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<SalesItem> salesItems = new ArrayList<>();
+    public String productType;
+    public String productDescription;
+    public double productPrice;
 
     public Product(){
 
     }
+
+    private Product(Builder b) {
+        this.productID = b.productID;
+        this.productType = b.productType;
+        this.productDescription = b.productDescription;
+        this.productPrice = b.productPrice;
+        this.productName = b.productName;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
     public String getProductType() {
         return productType;
     }
@@ -42,11 +52,6 @@ public class Product implements Serializable {
         return productPrice;
     }
 
-
-    public String getProductID() {
-        return productID;
-    }
-
     public String getProductName() {
         return productName;
     }
@@ -56,7 +61,7 @@ public class Product implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
-        return Double.compare(product.productPrice, productPrice) == 0 && Objects.equals(productID, product.productID) && Objects.equals(productName, product.productName) && Objects.equals(productType, product.productType) && Objects.equals(productDescription, product.productDescription);
+        return Objects.equals(productID, product.productID) && Objects.equals(productPrice, product.productPrice) && Objects.equals(productName, product.productName) && Objects.equals(productType, product.productType) && Objects.equals(productDescription, product.productDescription);
     }
 
     @Override
@@ -67,62 +72,55 @@ public class Product implements Serializable {
     @Override
     public String toString() {
         return "Bundle{" +
-                "productType='" + productType + '\'' +
+                ", productID='" + productID + '\'' +
                 ", productDescription='" + productDescription + '\'' +
                 ", productPrice=" + productPrice +
-                ", productID='" + productID + '\'' +
+                "productType='" + productType + '\'' +
                 ", productName='" + productName + '\''
                 + '}';
     }
 
-    private Product(Product.Builder p) {
-        this.productType = p.productType;
-        this.productDescription = p.productDescription;
-        this.productPrice = p.productPrice;
-        this.productID = p.productID;
-        this.productName = p.productName;
-    }
+
     public static class Builder {
+        public String productID;
         private String productType;
         private String productDescription;
         private double productPrice;
-        public String productID;
         public String productName;
 
-        public Product.Builder setProductType(String productType) {
+        public Builder setProductID(String productID) {
+            this.productID = productID;
+            return this;
+
+        }
+        public Builder setProductType(String productType) {
             this.productType = productType;
             return this;
         }
 
-        public Product.Builder setProductDescription(String productDescription) {
+        public Builder setProductDescription(String productDescription) {
             this.productDescription = productDescription;
             return this;
 
         }
 
-        public Product.Builder setProductPrice(double productPrice) {
+        public Builder setProductPrice(double productPrice) {
             this.productPrice = productPrice;
             return this;
 
         }
 
-        public Product.Builder setProductID(String productID) {
-            this.productID = productID;
-            return this;
-
-        }
-
-        public Product.Builder setProductName(String productName) {
+        public Builder setProductName(String productName) {
             this.productName = productName;
             return this;
 
         }
 
-        public Product.Builder copy(Product product) {
+        public Builder copy(Product product) {
+            this.productID = product.productID;
             this.productType = product.productType;
             this.productDescription = product.productDescription;
             this.productPrice = product.productPrice;
-            this.productID = product.productID;
             this.productName = product.productName;
             return this;
         }
