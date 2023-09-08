5313f93ef7f06c4077c94c361f1cfaceb572b953
Habbo420 <100413007+Habbo420@users.noreply.github.com>
2023-09-08 15:40:19 +0200

    Update EnquiryFactory.java

--- src/main/java/za/ac/cput/factory/EnquiryFactory.java
+++ src/main/java/za/ac/cput/factory/EnquiryFactory.java
@@ -2,16 +2,17 @@ package za.ac.cput.factory;
 /*
 Author: Hanno Visser Immelman 221074414
 */
+import za.ac.cput.domain.Customer;
 import za.ac.cput.domain.Enquiry;
 import za.ac.cput.util.Helper;
 public class EnquiryFactory {
-    public static Enquiry buildEnquiry(String customerID, String enquiryName, String enquirySubjectLine, String enquiryBodyContent){
-        if (Helper.isNullOrEmpty(customerID) || Helper.isNullOrEmpty(enquiryName) || Helper.isNullOrEmpty(enquirySubjectLine) || Helper.isNullOrEmpty(enquiryBodyContent))
+    public static Enquiry buildEnquiry(Customer customer, String enquiryName, String enquirySubjectLine, String enquiryBodyContent){
+        if (Helper.isNullOrEmpty(enquiryName) || Helper.isNullOrEmpty(enquirySubjectLine) || Helper.isNullOrEmpty(enquiryBodyContent))
             return null;
         String enquiryID = Helper.generateID();
 
         return new Enquiry.Builder().setEnquiryID(enquiryID)
-                .setCustomerID(customerID)
+                .setCustomer(customer)
                 .setEnquiryName(enquiryName)
                 .setEnquirySubjectLine(enquirySubjectLine)
                 .setEnquiryBodyContent(enquiryBodyContent)
