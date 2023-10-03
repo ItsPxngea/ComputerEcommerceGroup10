1d1dfa8e04d4a89694e51f5d073f333e8e16a437
David Garrancho <Garranchod@gmail.com>
2023-10-03 14:43:09 +0200

    Security Stuff so far

---
+++ src/main/java/za/ac/cput/config/SpringSecurityConfig.java
@@ -0,0 +1,50 @@
+package za.ac.cput.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import za.ac.cput.config.jwt.JwtAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter ;
+    private final MyCustomerDetailsService myCustomerDetailsService ;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests(authorizeRequests -> {
+                    authorizeRequests
+                            .requestMatchers("/auth/register", "/auth/login", "/post/seach").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .cors(cors -> cors.disable())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
+    { return authenticationConfiguration.getAuthenticationManager();}
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    { return new BCryptPasswordEncoder(); }
+
+}
